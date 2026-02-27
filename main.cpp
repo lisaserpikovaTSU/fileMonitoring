6d04ad6c8114d334df6464030fede5b6fd21fecf
lisa <liza.serpikova@stud.tsu.ru>
2026-02-27 14:33:32 +0700

    test work with QFileInfo

--- main.cpp
+++ main.cpp
@@ -1,19 +1,42 @@
 #include <QCoreApplication>
+#include <QFileInfo>
+#include <QString>
+#include <QTextStream>
+#include <QTimer>
+#include <thread>
+#include <chrono>
 
-int main(int argc, char *argv[])
+QTextStream cin(stdin);
+QTextStream cout(stdout);
+
+void CheckFileState(QFileInfo& file) {
+    file.refresh();
+    if (file.exists()) {
+        cout << "File exist, size: " << file.size() << Qt::endl;
+    } else {
+        cout << "File not exist" << Qt::endl;
+    }
+}
+
+int main()
 {
-    QCoreApplication a(argc, argv);
 
-    // Set up code that uses the Qt event loop here.
-    // Call a.quit() or a.exit() to quit the application.
-    // A not very useful example would be including
-    // #include <QTimer>
-    // near the top of the file and calling
-    // QTimer::singleShot(5000, &a, &QCoreApplication::quit);
-    // which quits the application after 5 seconds.
+    QString path;
+    cout << "Input file path: " << Qt::endl;
+    path = cin.readLine();
 
-    // If you do not need a running Qt event loop, remove the call
-    // to a.exec() or use the Non-Qt Plain C++ Application template.
+    QFileInfo file(path);
 
-    return a.exec();
+    if (!file.isFile()) {
+        cout << "Incorrect path" << Qt::endl;
+        return 1;
+    }
+
+    while (true) {
+        CheckFileState(file);
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+    }
+
+    return 0;
+    // для проверки: /Users/liza/Desktop/hint.png
 }
