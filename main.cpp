25a78090091fbd0c83ab1bfd66a8ad21da6ce2d7
lisa <liza.serpikova@stud.tsu.ru>
2026-03-06 14:00:02 +0700

    test work with signal-slot

--- main.cpp
+++ main.cpp
@@ -5,9 +5,39 @@
 #include <QTimer>
 #include <thread>
 #include <chrono>
+#include <QObject>
 
 QTextStream cin(stdin);
 QTextStream cout(stdout);
+
+class nabludatel : public QObject
+{
+    Q_OBJECT
+
+public:
+    nabludatel(QString& path) { file(path); } //исправить
+
+public slots:
+    void checkState(QFileInfo&);
+
+signals:
+    void stateChanged();
+
+private:
+    QFileInfo file;
+};
+
+void nabludatel::checkState(QFileInfo& file) {
+    QFileInfo file2 = file;
+    file.refresh();
+    if (file != file2) {
+        emit stateChanged();
+    }
+}
+
+void nabludatel::stateChanged() {
+    cout << "File changed" << Qt::endl;
+}
 
 void CheckFileState(QFileInfo& file) {
     file.refresh();
@@ -25,10 +55,13 @@ int main()
     cout << "Input file path: " << Qt::endl;
     path = cin.readLine();
 
+    nabludatel f(path);
+
+    /*
     QFileInfo file(path);
 
-    if (!file.isFile()) {
-        cout << "Incorrect path" << Qt::endl;
+    if (file.isDir()) {
+        cout << "Incorrect path, you've inputed a directory" << Qt::endl;
         return 1;
     }
 
@@ -36,6 +69,7 @@ int main()
         CheckFileState(file);
         std::this_thread::sleep_for(std::chrono::seconds(2));
     }
+    */
 
     return 0;
     // для проверки: /Users/liza/Desktop/hint.png
